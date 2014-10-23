2bb247a04d966d175a479a203a8d7d48075b0ba1
Jeroen Veldhuijzen <veldmuijz@gmail.com>
2014-10-23 19:26:01 +0200

    Enabled chat messages on the homepage for testing

---
+++ GSNChat.Frontend/app/controllers/ChatController.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+app.controller('chatController', ['$scope', '$location', 'chatService', function ($scope, $location, chatService) {
+
+    
+
+    $scope.message = "";
+
+    $scope.login = function () {
+
+        authService.login($scope.loginData).then(function (response) {
+
+            $location.path('/orders');
+
+        },
+         function (err) {
+             $scope.message = err.error_description;
+         });
+    };
+
+}]);
--- GSNChat.Frontend/app/controllers/homeController.js
+++ GSNChat.Frontend/app/controllers/homeController.js
@@ -1,4 +1,38 @@
 ﻿'use strict';
 app.controller('homeController', ['$scope', function ($scope) {
 
+    alert("Loaded")
+    var serviceBase = 'http://localhost:41021/';
+    var hub = 'http://localhost:41021/';
+
+    var chat = $.connection.chatHub;
+    $.connection.hub.url = 'http://localhost:41021/signalr';
+
+    chat.client.broadcastMessage = function (name, message) {
+        alert(name, message);
+
+
+    };
+
+    //Have to add jsonp = true for crossdomain requests
+    $.connection.hub.start({ jsonp: true }).done(function () {
+        alert("Connected!")
+        $('#sendmessage').click(function () {
+            // Call the Send method on the hub.
+            chat.server.send($('#displayname').val(), $('#message').val());
+            // Clear text box and reset focus for next comment.
+            $('#message').val('').focus();
+        });
+    });
+
+    $scope.loginData = {
+        userName: "",
+        password: ""
+    };
+
+
+    $scope.receiveMessage = function () {
+
+    };
+
 }]);
