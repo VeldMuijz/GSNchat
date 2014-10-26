4ff3949380e27ba37de76b648fe6166391d3b425
Jeroen Veldhuijzen <veldmuijz@gmail.com>
2014-10-26 21:18:30 +0100

    Added search bar to filter on users

--- GSNChat.Frontend/app/controllers/ChatController.js
+++ GSNChat.Frontend/app/controllers/ChatController.js
@@ -1,58 +1,45 @@
 ﻿'use strict';
 app.controller('chatController', ['$scope', '$location', 'chatService', 'authService', function ($scope, $location, chatService, authService) {
 
-    $scope.backend = 'http://localhost:41021';
+    
 
     if (!authService.authentication.isAuth) {
         //no authenticated or not longer authenticated
         alert("U bent niet (langer) aangemeld.")
         $location.path('/login');
 
-
     } else {
+
+        var chat = $.connection.chatHub;
+        $scope.backend = 'http://localhost:41021';
+        $.connection.hub.url = $scope.backend + '/signalr/hubs';
         $scope.message = '';
         $scope.chatStore = chatService.getStore();
-
         $scope.userName = authService.authentication.userName;
+
         $scope.sendMessage = function () {
-            // Call the Send method on the hub.
-            //chat.server.send($scope.userName, $('#chatmessage').val());
-
             chatService.sendMessage($scope.userName, $('#chatmessage').val());
-
             // Clear text box and reset focus for next comment.
             $('#chatmessage').val('').focus();
         }
 
-        var chat = $.connection.chatHub;
-        $.connection.hub.url = $scope.backend + '/signalr/hubs';
-
         chat.client.broadcastMessage = function (name, message) {
-
             var chatObject = { "user": name, "message": message, "timestamp": "14:00", "groupId": "" };
 
-            
+            //use apply to update view immediately
             $scope.$apply(function () {
                 chatService.storeMessage(chatObject);
                 $scope.chatStore = chatService.getStore();
 
             });
-            // Old logic to append to chatfeed:$('#chatboxfeed').append("<p class=\"row\"><div class=\"col-sm-2 \">" + name + ":</div><div class=\"col-sm-8 \">" + message + "</div><div class=\"col-sm-2 right \">20-10-2014 08:00</div</p>");
 
         };
 
+        //Make connection to SignalR backend
         //Have to add jsonp = true for crossdomain requests
         $.connection.hub.start({ jsonp: true }).done(function () {
 
-            //$('#sendmessage').click(function () {
-            //    // Call the Send method on the hub.
-            //    //chat.server.send($scope.userName, $('#chatmessage').val());
 
-            //    chatService.sendMessage($scope.userName, $('#chatmessage').val());
-
-            //    // Clear text box and reset focus for next comment.
-            //    $('#chatmessage').val('').focus();
-            //});
         });
 
     }
