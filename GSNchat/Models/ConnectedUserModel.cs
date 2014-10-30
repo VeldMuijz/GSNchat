32b3a443d888a5faf14cd03cfbb1a330921d734a
Jeroen Veldhuijzen <veldmuijz@gmail.com>
2014-10-30 22:11:50 +0100

    Needed a connected user class

---
+++ GSNchat/Models/ConnectedUserModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GSNchat.Models
+{
+    public class ConnectedUserModel
+    {
+        public String Name { get; set; }
+        public HashSet<string> ConnectionID { get; set; }
+    }
+}
