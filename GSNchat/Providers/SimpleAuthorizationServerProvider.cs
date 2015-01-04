29a24a21b8252cca75661fd0b62bd8fad5a01416
Jeroen Veldhuijzen <veldmuijz@gmail.com>
2015-01-04 17:28:16 +0100

    Correct user/pass validation and role assignment

--- GSNchat/Providers/SimpleAuthorizationServerProvider.cs
+++ GSNchat/Providers/SimpleAuthorizationServerProvider.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.Owin.Security.OAuth;
+﻿using GSNchat.Models;
+using Microsoft.Owin.Security.OAuth;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
+using Newtonsoft.Json;
+using System.Web.Helpers;
 
 namespace GSNchat
 {
@@ -34,16 +37,35 @@ namespace GSNchat
 
             var orchestrate = new Orchestrate.Net.Orchestrate("0b42c04c-0d70-4da8-a3c1-2036882369d0");
             var result = orchestrate.Search("users", context.UserName, 1);
+            
             if (result.Count == 0) {
                         context.SetError("invalid_grant", "The user name or password is incorrect.");
                        return;
             }
 
-            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
-            identity.AddClaim(new Claim("sub", context.UserName));
-            identity.AddClaim(new Claim("role", "user"));
+            var userResult = result.Results.FirstOrDefault();
+            UserModel user = JsonConvert.DeserializeObject<UserModel>(userResult.Value.ToString());
 
-            context.Validated(identity);
+            if (Crypto.VerifyHashedPassword(user.Password, context.Password))
+            {
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+
+                identity.AddClaim(new Claim("sub", context.UserName));
+                if (!String.IsNullOrEmpty(user.Role))
+                {
+                    identity.AddClaim(new Claim("role", user.Role));
+                }
+                else {
+                    identity.AddClaim(new Claim("role", "User"));
+                }
+
+                context.Validated(identity);
+            }
+            else {
+                return;
+            }
+            
+            
 
         }
     }
